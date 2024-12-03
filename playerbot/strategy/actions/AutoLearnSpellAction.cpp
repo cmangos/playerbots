fb37d28e2af24099463728e4c8b040675e27a1cf
mostlikely4r <sebastiaan.keek@gmail.com>
2024-12-03 09:19:19 +0100

    Auto learn: Spells learned from quests are no longer limited to quests above min level 9

--- playerbot/strategy/actions/AutoLearnSpellAction.cpp
+++ playerbot/strategy/actions/AutoLearnSpellAction.cpp
@@ -132,7 +132,7 @@ void AutoLearnSpellAction::LearnQuestSpells(std::ostringstream* out)
         uint32 questId = i->first;
         Quest const* quest = i->second.get();
 
-        if (!quest->GetRequiredClasses() || quest->IsRepeatable() || quest->GetMinLevel() < 10)
+        if (!quest->GetRequiredClasses() || quest->IsRepeatable())
             continue;
 
         if (!bot->SatisfyQuestClass(quest, false) ||

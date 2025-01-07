1f2e122919790c021434bb1e521e8347d02e34aa
ile <59745756+ileboii@users.noreply.github.com>
2025-01-07 10:26:50 +0200

    Ai Chat: Custom prompts loadable from database. (#115)
    * Ai Chat: Custom prompts loadable from database.
    
    * Fix: Use pre-prompt instead of prompt

--- playerbot/strategy/actions/SayAction.cpp
+++ playerbot/strategy/actions/SayAction.cpp
@@ -526,8 +526,10 @@ void ChatReplyAction::ChatReplyDo(Player* bot, uint32 type, uint32 guid1, uint32
 
                 placeholders["<initial message>"] = msg;
 
+                std::string llmPromptCustom = AI_VALUE(std::string, "manual saved string::llmdefaultprompt");
+
                 std::map<std::string, std::string> jsonFill;
-                jsonFill["<pre prompt>"] = sPlayerbotAIConfig.llmPrePrompt;
+                jsonFill["<pre prompt>"] = sPlayerbotAIConfig.llmPrePrompt + " " + llmPromptCustom;
                 jsonFill["<prompt>"] = sPlayerbotAIConfig.llmPrompt;
                 jsonFill["<post prompt>"] = sPlayerbotAIConfig.llmPostPrompt;
 

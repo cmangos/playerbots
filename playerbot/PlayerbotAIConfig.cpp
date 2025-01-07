775b6e5a9440047b111a8e1aa5b1c1f66a893434
mostlikely4r <sebastiaan.keek@gmail.com>
2025-01-07 16:49:04 +0100

    Ai chat: Removed slash output from the llm

--- playerbot/PlayerbotAIConfig.cpp
+++ playerbot/PlayerbotAIConfig.cpp
@@ -652,8 +652,8 @@ bool PlayerbotAIConfig::Initialize()
     llmPostPrompt = config.GetStringDefault("AiPlayerbot.LLMPostPrompt", "<sender name>:");
 
     llmResponseStartPattern = config.GetStringDefault("AiPlayerbot.LLMResponseStartPattern", R"(("text":\s*"))");
-    llmResponseDeletePattern = config.GetStringDefault("AiPlayerbot.LLMResponseDeletePattern", R"((\\n|<sender name>:))");
     llmResponseEndPattern = config.GetStringDefault("AiPlayerbot.LLMResponseEndPattern", R"(("|\b(?!<sender name>\b)(\w+):))");
+    llmResponseDeletePattern = config.GetStringDefault("AiPlayerbot.LLMResponseDeletePattern", R"((\\n|<sender name>:|\\[^ ]+))");
     llmResponseSplitPattern = config.GetStringDefault("AiPlayerbot.LLMResponseSplitPattern", R"((\*.*?\*)|(\[.*?\])|(\'.*\')|([^\*\[\] ][^\*\[\]]+?[.?!]))");
 
     if (false) //Disable for release

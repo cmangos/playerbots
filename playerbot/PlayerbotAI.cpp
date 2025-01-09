0b29c22b8e0f9fd27ace5cdd06a7f537d0891bc7
mostlikely4r <sebastiaan.keek@gmail.com>
2025-01-09 21:28:19 +0100

    AI chat: Prevent ai chat from running rampant with alts

--- playerbot/PlayerbotAI.cpp
+++ playerbot/PlayerbotAI.cpp
@@ -1621,7 +1621,7 @@ void PlayerbotAI::HandleBotOutgoingPacket(const WorldPacket& packet)
                 if (!isAiChat || isFromFreeBot)
                 {
                     // random bot speaks, chat CD
-                    if (isFromFreeBot && isPaused)
+                    if ((isFromFreeBot || isAiChat) && isPaused)
                         return;
 
                     // BG: react only if mentioned or if not channel and real player spoke

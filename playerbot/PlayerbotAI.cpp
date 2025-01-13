e45711a1ae71f58f4bd01d18d1288439c71f0d5e
mostlikely4r <sebastiaan.keek@gmail.com>
2025-01-13 16:03:32 +0100

    Combat: Bots now only react faster when entering combat if they are actually drinking

--- playerbot/PlayerbotAI.cpp
+++ playerbot/PlayerbotAI.cpp
@@ -884,7 +884,7 @@ void PlayerbotAI::OnCombatStarted()
         aiObjectContext->GetValue<bool>("has attackers")->Reset();
 
         ChangeEngine(BotState::BOT_STATE_COMBAT);
-        if (bot->IsSitState());
+        if (bot->IsSitState())
         {
             ResetAIInternalUpdateDelay();
         }

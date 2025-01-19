b0945cd3decd9ff044b4ec049cb57567bc4d9a0d
ile <59745756+ileboii@users.noreply.github.com>
2025-01-19 09:46:41 +0200

    Update ArmsWarriorStrategy.cpp

--- playerbot/strategy/warrior/ArmsWarriorStrategy.cpp
+++ playerbot/strategy/warrior/ArmsWarriorStrategy.cpp
@@ -1049,6 +1049,8 @@ public:
         const std::string& actionName = action->getName();
         if ((actionName == "berserker stance" ||
             actionName == "whirlwind" ||
+            actionName == "mortal strike" ||
+            actionName == "heroic strike" ||
             actionName == "cleave") &&
             AI_VALUE2(bool, "trigger active", "melee light aoe") &&
             bot->HasSpell(12292) &&
@@ -1073,6 +1075,8 @@ public:
         const std::string& actionName = action->getName();
         if ((actionName == "berserker stance" ||
             actionName == "whirlwind" ||
+            actionName == "mortal strike" ||
+            actionName == "heroic strike" ||
             actionName == "cleave") &&
             AI_VALUE2(bool, "trigger active", "melee light aoe") &&
             bot->HasSpell(12292) &&

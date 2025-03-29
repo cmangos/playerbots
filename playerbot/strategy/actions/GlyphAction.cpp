bb64ec8b666af51d02601fdf3100ef63cfd597ec
Sh4p3 <gheysens@hotmail.fr>
2025-03-29 15:37:40 +0100

    strategy: prevent alt bots from swapping glyphs

--- playerbot/strategy/actions/GlyphAction.cpp
+++ playerbot/strategy/actions/GlyphAction.cpp
@@ -203,6 +203,9 @@ uint8 SetGlyphAction::GetBestSlotForGlyph(uint32 glyphId, Player* bot, uint8 wan
 
             uint32 currentGlyphId = bot->GetGlyph(glyphIndex);
 
+            if (currentGlyphId && !sRandomPlayerbotMgr.IsRandomBot(bot))
+                continue;
+
             const ItemPrototype* currentGlyphProto = GetGlyphProtoFromGlyphId(currentGlyphId, bot->getClass());
 
             if (!currentGlyphProto) //Empty slot put it here.

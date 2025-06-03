1c4a8e0e95ead33fa0f355e5dd2707eb1ffd42b5
mostlikely4r <sebastiaan.keek@gmail.com>
2025-06-03 16:43:10 +0200

    Travel: Fixed distance calculations to places on different maps

--- playerbot/TravelMgr.cpp
+++ playerbot/TravelMgr.cpp
@@ -2359,11 +2359,11 @@ std::vector<std::pair<WorldPosition, float>> TravelMgr::sqMapTransDistances(cons
 
     for (auto& mapTrans : mapTransfers->second)
     {
-        WorldPosition realEnd = mapTrans.GetPointTo();
+        WorldPosition portalOnEndMap = mapTrans.GetPointTo();
 
-        float sqDist = mapTrans.Distance(start, realEnd);
+        float sqDist = mapTrans.sqDist(start, portalOnEndMap);
 
-        retPortals.push_back(std::make_pair(realEnd, sqDist));
+        retPortals.push_back(std::make_pair(portalOnEndMap, sqDist));
     }
 
     return retPortals;
@@ -2371,6 +2371,7 @@ std::vector<std::pair<WorldPosition, float>> TravelMgr::sqMapTransDistances(cons
 
 float TravelMgr::MapTransDistance(const WorldPosition& start, const WorldPosition& end, bool toMap) const
 {
+    //These are the portals on the end map that start can reach.
     std::vector<std::pair<WorldPosition, float>> portals = sqMapTransDistances(start, end.getMapId());
 
     if (portals.empty())
@@ -2378,9 +2379,9 @@ float TravelMgr::MapTransDistance(const WorldPosition& start, const WorldPositio
 
     float minsqDist = FLT_MAX;
 
-    for (auto& [portal, distance] : portals)
+    for (auto& [portal, sqDistanceFromStartToPortal] : portals)
     {
-        float sqDist = portal.sqDistance2d(end);
+        float sqDist = sqDistanceFromStartToPortal + portal.sqDistance2d(end);
         if (sqDist < minsqDist)
             minsqDist = sqDist;
     }

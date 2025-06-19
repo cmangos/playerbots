99a9982add72f895a372f8840a09759f51dbfa4e
Sh4p3 <gheysens@hotmail.fr>
2025-06-19 17:35:23 +0200

    Strategy: Warrior charge tweak added to other expansions

--- playerbot/strategy/warrior/FuryWarriorStrategy.cpp
+++ playerbot/strategy/warrior/FuryWarriorStrategy.cpp
@@ -399,7 +399,7 @@ void FuryWarriorStrategy::InitCombatTriggers(std::list<TriggerNode*>& triggers)
 
     triggers.push_back(new TriggerNode(
         "enemy out of melee",
-        NextAction::array(0, new NextAction("charge", ACTION_MOVE), NULL)));
+        NextAction::array(0, new NextAction("charge", ACTION_MOVE + 1), NULL)));
 
     triggers.push_back(new TriggerNode(
         "target critical health",
@@ -751,7 +751,7 @@ void FuryWarriorStrategy::InitCombatTriggers(std::list<TriggerNode*>& triggers)
 
     triggers.push_back(new TriggerNode(
         "enemy out of melee",
-        NextAction::array(0, new NextAction("charge", ACTION_MOVE), NULL)));
+        NextAction::array(0, new NextAction("charge", ACTION_MOVE + 1), NULL)));
 
     triggers.push_back(new TriggerNode(
         "instant slam",

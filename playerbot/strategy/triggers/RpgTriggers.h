54413f0e57447fb556452b720e42ddb93325e774
mostlikely4r <sebastiaan.keek@gmail.com>
2025-10-06 21:25:00 +0200

    Performance: Reverted interaction distance change for using items during rpg.
    This reverts commit 5dfdda8708c2a10f20209047146c916da63651bc.

--- playerbot/strategy/triggers/RpgTriggers.h
+++ playerbot/strategy/triggers/RpgTriggers.h
@@ -30,9 +30,8 @@ namespace ai
         virtual std::vector<std::string> GetUsedTriggers() { return { "no rpg target" }; }
         virtual std::vector<std::string> GetUsedValues() { return {"distance", "rpg target"}; }
 #endif
-        virtual bool IsActive() { return !NoRpgTargetTrigger::IsActive() && AI_VALUE2(float, "distance", "rpg target") > maxDistance; };
-    protected:
-        float maxDistance = INTERACTION_DISTANCE;
+
+        virtual bool IsActive() { return !NoRpgTargetTrigger::IsActive() && AI_VALUE2(float, "distance", "rpg target") > INTERACTION_DISTANCE; };
     };
 
     class NearRpgTargetTrigger : public FarFromRpgTargetTrigger
@@ -239,7 +238,7 @@ namespace ai
     class RpgItemTrigger : public RpgTrigger
     {
     public:
-        RpgItemTrigger(PlayerbotAI* ai, std::string name = "rpg item") : RpgTrigger(ai, name) { maxDistance = INTERACTION_DISTANCE * 2;}
+        RpgItemTrigger(PlayerbotAI* ai, std::string name = "rpg item") : RpgTrigger(ai, name) {}
         virtual bool IsActive();
     };
 

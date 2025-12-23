c6346f87e6f0a2ee2f583130602a0e352965c476
mostlikely4r <sebastiaan.keek@gmail.com>
2025-12-23 13:58:06 +0100

    Target: Fixed old code checking low hp target.

--- playerbot/strategy/values/DpsTargetValue.cpp
+++ playerbot/strategy/values/DpsTargetValue.cpp
@@ -1,40 +1,10 @@
 
 #include "playerbot/playerbot.h"
 #include "DpsTargetValue.h"
+#include "LeastHpTargetValue.h"
 
 using namespace ai;
 
-class FindLeastHpTargetStrategy : public FindNonCcTargetStrategy
-{
-public:
-    FindLeastHpTargetStrategy(PlayerbotAI* ai) : FindNonCcTargetStrategy(ai)
-    {
-        minHealth = 0;
-    }
-
-public:
-    virtual void CheckAttacker(Unit* attacker, ThreatManager* threatManager) override
-    {
-        // do not use this logic for pvp
-        if (attacker->IsPlayer())
-            return;
-
-        if (IsCcTarget(attacker)) return;
-
-        Group* group = ai->GetBot()->GetGroup();
-        if (group)
-        {
-            uint64 guid = group->GetTargetIcon(4);
-            if (guid && attacker->GetObjectGuid() == ObjectGuid(guid))
-                return;
-        }
-        if (!result || result->GetHealth() > attacker->GetHealth())
-            result = attacker;
-    }
-
-protected:
-    float minHealth;
-};
 
 Unit* DpsTargetValue::Calculate()
 {
--- playerbot/strategy/values/LeastHpTargetValue.h
+++ playerbot/strategy/values/LeastHpTargetValue.h
@@ -4,6 +4,37 @@
 
 namespace ai
 {
+    class FindLeastHpTargetStrategy : public FindNonCcTargetStrategy
+    {
+    public:
+        FindLeastHpTargetStrategy(PlayerbotAI* ai) : FindNonCcTargetStrategy(ai)
+        {
+            minHealth = 0;
+        }
+    public:
+        virtual void CheckAttacker(Unit* attacker, ThreatManager* threatManager) override
+        {
+            // do not use this logic for pvp
+            if (attacker->IsPlayer())
+                return;
+
+            if (IsCcTarget(attacker))
+                return;
+
+            Group* group = ai->GetBot()->GetGroup();
+            if (group)
+            {
+                uint64 guid = group->GetTargetIcon(4);
+                if (guid && attacker->GetObjectGuid() == ObjectGuid(guid))
+                    return;
+            }
+            if (!result || result->GetHealth() > attacker->GetHealth())
+                result = attacker;
+        }
+    protected:
+        float minHealth;
+    };
+
     class LeastHpTargetValue : public TargetValue
 	{
 	public:

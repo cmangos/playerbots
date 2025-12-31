d063da4c62c3aa30c188be314258710e390aeb03
ile <59745756+ileboii@users.noreply.github.com>
2025-12-31 06:33:17 +0200

    Wander: Lower following threshold

--- playerbot/strategy/triggers/RangeTriggers.h
+++ playerbot/strategy/triggers/RangeTriggers.h
@@ -423,7 +423,7 @@ namespace ai
                     return false;
 
                 float dist = AI_VALUE2(float, "distance", "master target");
-                return sServerFacade.IsDistanceGreaterThan(dist, sPlayerbotAIConfig.followDistance + 8.0f) && sServerFacade.IsDistanceLessOrEqualThan(dist, 50.0f) && master->IsMoving();
+                return sServerFacade.IsDistanceGreaterThan(dist, sPlayerbotAIConfig.followDistance + 5.0f) && sServerFacade.IsDistanceLessOrEqualThan(dist, 50.0f) && master->IsMoving();
             }
             return false;
         }

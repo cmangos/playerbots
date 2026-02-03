29b03ee68f410c98345857e1782391fcfb218d77
Badger <BadgerByNature@gmail.com>
2026-02-02 22:49:01 -0600

    Updates the logic to only detect totems in the bot's group or raid subgroup. Totems are not raid-wide in Vanilla or TBC. This allows separate groups in the same raid to use the same totem, e.g. groups 1 and 2 both dropping Windfury.

--- playerbot/strategy/values/HasTotemValue.h
+++ playerbot/strategy/values/HasTotemValue.h
@@ -19,12 +19,37 @@ namespace ai
                 if (!unit)
                     continue;
 
-                Creature* creature = dynamic_cast<Creature*>(unit);
-                if (!creature || !creature->IsTotem())
+                Creature* totem = dynamic_cast<Creature*>(unit);
+                if (!totem || !totem->IsTotem())
+                    continue;
+                
+                const bool totemIsInRange = strstri(totem->GetName(), qualifier.c_str()) &&
+                    sServerFacade.GetDistance2d(bot, totem) <= ai->GetRange("spell");
+                
+                if (!totemIsInRange) continue;
+
+                Unit* totemOwner = totem->GetCreator(totem);
+                if (!totemOwner)
                     continue;
 
-                if (strstri(creature->GetName(), qualifier.c_str()) && sServerFacade.GetDistance2d(bot, creature) <= ai->GetRange("spell"))
-                    return true;
+                if (totemOwner == bot) return true; // Found our own totem in range
+
+                const Group* botGroup = bot->GetGroup();
+                if (!botGroup) continue;
+
+                if (const Player* totemPlayerOwner = totemOwner->IsPlayer() ? dynamic_cast<Player*>(totemOwner) : nullptr)
+                {
+#ifdef MANGOSBOT_TWO
+                    // Most totems are raid-wide in Wrath - Special handling should be added for the handful that aren't
+                    if (totemPlayerOwner->GetGroup() == botGroup) return true;
+#else
+                    // Totem from a player in our group found, and we are either not in a raid or in the same raid subgroup
+                    if (totemPlayerOwner->GetGroup() == botGroup)
+                    {
+                        if (!botGroup->IsRaidGroup() || botGroup->SameSubGroup(totemPlayerOwner, bot)) return true;
+                    }
+#endif
+                }
             }
 
             return false;

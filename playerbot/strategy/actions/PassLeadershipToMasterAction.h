6e1fc91700e5fe01f5be78db218b863524d9e4b7
mostlikely4r <sebastiaan.keek@gmail.com>
2026-03-25 23:31:58 +0100

    Group: Fix bots allowing to change leader to non-masters.

--- playerbot/strategy/actions/PassLeadershipToMasterAction.h
+++ playerbot/strategy/actions/PassLeadershipToMasterAction.h
@@ -8,17 +8,19 @@ namespace ai
     public:
         PassLeadershipToMasterAction(PlayerbotAI* ai, std::string name = "leader", std::string message = "Passing leader to you!") : ChatCommandAction(ai, name), message(message) {}
 
+        virtual Player* PassLeaderTo(Event& event) { return GetMaster(); };
+
         virtual bool Execute(Event& event) override
         {
-            Player* master = GetMaster();
-            if (master && master != bot && bot->GetGroup() && bot->GetGroup()->IsMember(master->GetObjectGuid()))
+            Player* passLeaderTo = PassLeaderTo(event);
+            if (passLeaderTo && passLeaderTo != bot && bot->GetGroup() && bot->GetGroup()->IsMember(passLeaderTo->GetObjectGuid()))
             {
                 WorldPacket p(SMSG_GROUP_SET_LEADER, 8);
-                p << master->GetObjectGuid();
+                p << passLeaderTo->GetObjectGuid();
                 bot->GetSession()->HandleGroupSetLeaderOpcode(p);
                 
                 if (!message.empty())
-                    ai->TellPlayerNoFacing(GetMaster(), message);
+                    ai->TellPlayerNoFacing(passLeaderTo, message);
 
                 if (sRandomPlayerbotMgr.IsRandomBot(bot))
                 {
@@ -48,9 +50,11 @@ namespace ai
     public:
         GiveLeaderAction(PlayerbotAI* ai, std::string message = "Lead the way!") : PassLeadershipToMasterAction(ai, "give leader", message) {}
 
+        virtual Player* PassLeaderTo(Event& event) { return event.getOwner(); };
+
         virtual bool isUseful() override
         {
-            return ai->HasRealPlayerMaster() && bot->GetGroup() && bot->GetGroup()->IsLeader(bot->GetObjectGuid());
-        }
+            return bot->GetGroup() && bot->GetGroup()->IsLeader(bot->GetObjectGuid());
+        }        
     };    
 }

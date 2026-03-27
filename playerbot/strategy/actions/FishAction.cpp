7e875cc573a57af8e7c3729be369c7fa6db4a426
mostlikely4r <sebastiaan.keek@gmail.com>
2026-03-27 22:34:08 +0100

    Fish: Fixed bots constantly trying to equip a fishingpole when they already have one equiped.

--- playerbot/strategy/actions/FishAction.cpp
+++ playerbot/strategy/actions/FishAction.cpp
@@ -124,7 +124,8 @@ bool FishAction::Execute(Event& event)
     uint8 bagIndex = pole->GetBagSlot();
     uint8 slot = pole->GetSlot();
 
-    EquipAction::EquipItem(ai, GetMaster(), pole);
+    if (slot != EQUIPMENT_SLOT_MAINHAND)
+        EquipAction::EquipItem(ai, GetMaster(), pole);
 
     Event fishCastEvent = Event("fish", "7731 " + chat->formatWorldobject(bot));
     bool didCast = CastCustomSpellAction::Execute(fishCastEvent);

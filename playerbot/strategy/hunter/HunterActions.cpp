16dabf28ffdb269bad6c32ee1c4ee0a0308864c3
Sagrid <sagrid.confluxorder@gmail.com>
2026-04-02 16:49:57 +0200

    Implement ammo equipping logic for Hunters
    Added functionality to equip the best available ammo for the hunter's ranged weapon.

--- playerbot/strategy/hunter/HunterActions.cpp
+++ playerbot/strategy/hunter/HunterActions.cpp
@@ -32,6 +32,83 @@ bool CastAutoShotAction::isUseful()
     return ai->HasStrategy("ranged", BotState::BOT_STATE_COMBAT) && AI_VALUE(uint32, "active spell") != AI_VALUE2(uint32, "spell id", getName());
 }
 
+bool HunterEquipAmmoAction::Execute(Event& event)
+{
+    // Get ranged weapon
+    Item* ranged = bot->GetItemByPos(INVENTORY_SLOT_BAG_0, EQUIPMENT_SLOT_RANGED);
+    if (!ranged)
+        return false;
+
+    uint32 ammoClass = ITEM_CLASS_PROJECTILE;
+    uint32 subClass = 0;
+
+    switch (ranged->GetProto()->SubClass)
+    {
+        case ITEM_SUBCLASS_WEAPON_GUN:
+            subClass = ITEM_SUBCLASS_BULLET;
+            break;
+        case ITEM_SUBCLASS_WEAPON_BOW:
+        case ITEM_SUBCLASS_WEAPON_CROSSBOW:
+            subClass = ITEM_SUBCLASS_ARROW;
+            break;
+        case ITEM_SUBCLASS_WEAPON_THROWN:
+            ammoClass = ITEM_CLASS_WEAPON;
+            subClass = ITEM_SUBCLASS_WEAPON_THROWN;
+            break;
+    }
+
+    uint32 currentAmmoId = bot->GetUInt32Value(PLAYER_AMMO_ID);
+
+    const ItemPrototype* bestAmmoProto = nullptr;
+
+    // Scan inventory
+    for (int i = INVENTORY_SLOT_BAG_START; i < INVENTORY_SLOT_BAG_END; ++i)
+    {
+        if (Bag* bag = (Bag*)bot->GetItemByPos(INVENTORY_SLOT_BAG_0, i))
+        {
+            for (uint32 j = 0; j < bag->GetBagSize(); ++j)
+            {
+                if (Item* item = bag->GetItemByPos(j))
+                {
+                    const ItemPrototype* proto = item->GetProto();
+                    if (!proto)
+                        continue;
+
+                    if (proto->Class == ammoClass && proto->SubClass == subClass)
+                    {
+                        float betterAmmoStacks = BetterStacks(proto, "ammo");
+
+                        // Best ammo possible (nothing better exists)
+                        if (betterAmmoStacks <= 0)
+                        {
+                            bestAmmoProto = proto;
+                            break;
+                        }
+
+                        // Fallback if no better found yet
+                        if (!bestAmmoProto)
+                            bestAmmoProto = proto;
+                    }
+                }
+            }
+
+            // Stop outer loop if best ammo found
+            if (bestAmmoProto && BetterStacks(bestAmmoProto, "ammo") <= 0)
+                break;
+        }
+    }
+
+    // Equip best ammo found
+    if (bestAmmoProto && currentAmmoId != bestAmmoProto->ItemId)
+    {
+        bot->SetUInt32Value(PLAYER_AMMO_ID, bestAmmoProto->ItemId);
+        bot->UpdateDamagePhysical(RANGED_ATTACK);
+        return true;
+    }
+
+    return false;
+}
+
 bool CastAutoShotAction::Execute(Event& event)
 {
     if (!bot->IsStopped())

018bad5be66126ae91b3335e7e8f8acffd1cddd7
mostlikely4r <sebastiaan.keek@gmail.com>
2026-04-24 14:57:34 +0200

    Test: Fixed bot movement activity during tests.

--- playerbot/PlayerbotAI.cpp
+++ playerbot/PlayerbotAI.cpp
@@ -6072,6 +6072,7 @@ bool PlayerbotAI::AllowActive(ActivityType activityType)
         case ActivePiorityType::VISIBLE_FOR_PLAYER:
         case ActivePiorityType::IN_COMBAT:
         case ActivePiorityType::NEARBY_PLAYER:
+        case ActivePiorityType::IS_RUNNING_TEST:
             return true;
             break;
         case ActivePiorityType::IS_ALWAYS_ACTIVE:
@@ -6097,6 +6098,7 @@ bool PlayerbotAI::AllowActive(ActivityType activityType)
         case ActivePiorityType::IN_GROUP_WITH_REAL_PLAYER:
         case ActivePiorityType::IN_INSTANCE:
         case ActivePiorityType::IS_ALWAYS_ACTIVE:
+        case ActivePiorityType::IS_RUNNING_TEST:
             return true;
         case ActivePiorityType::VISIBLE_FOR_PLAYER:
             if (sPlayerbotAIConfig.forceActiveWhenNearPlayer)

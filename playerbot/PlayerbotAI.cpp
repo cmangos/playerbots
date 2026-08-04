993bfc5c87d557cfe9f04dd5fa0bb0317c90fb4d
Velite12 <45871932+Velite12@users.noreply.github.com>
2026-08-04 16:45:43 -0400

    Don't cast a damage spell if target is immune to it (#349)

--- playerbot/PlayerbotAI.cpp
+++ playerbot/PlayerbotAI.cpp
@@ -4176,15 +4176,30 @@ bool PlayerbotAI::CanCastSpell(uint32 spellid, Unit* target, uint8 effectMask, b
             }
         }
 
+        bool immune = target->IsImmuneToSpell(spellInfo, false, effectMask, bot);
         if (!damage)
         {
-            bool immune = target->IsImmuneToSpell(spellInfo, false, effectMask, bot);
             if (!immune)
             {
                 for (int32 i = EFFECT_INDEX_0; i <= EFFECT_INDEX_2; i++)
                     immune = target->IsImmuneToSpellEffect(spellInfo, SpellEffectIndex(i), false);
             }
 
+            if (immune)
+            {
+                if (checkResult)
+                {
+                    *checkResult = SPELL_FAILED_IMMUNE;
+                }
+
+                return false;
+            }
+        }
+        else
+        {
+            if (!immune)
+                immune = target->IsImmuneToDamage(GetSpellSchoolMask(spellInfo));
+                
             if (immune)
             {
                 if (checkResult)

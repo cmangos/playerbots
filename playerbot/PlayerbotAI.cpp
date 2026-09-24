4cf72baeb469266781461787d1c1afb5d9abf3e2
Velite12 <45871932+Velite12@users.noreply.github.com>
2026-09-24 05:57:54 -0400

    [Feature] Skip mobs that will not attack bots for looting (#374)
    * Skip mobs that will not attack bots for looting
    
    Apparently the unfriendly units finder was tagging 0/3000 neutral mobs as hostile, so after killing Captain Skarloc, where Thrall was standing he was apparently blocking them from looting. This should prevent it.
    
    * just check not hostile

--- playerbot/PlayerbotAI.cpp
+++ playerbot/PlayerbotAI.cpp
@@ -7995,7 +7995,7 @@ std::list<Unit*> PlayerbotAI::GetAllHostileNPCNonPetUnitsAroundWO(WorldObject* w
             if (hostileUnit->IsCreature())
             {
                 Creature* creature = GetCreature(hostileUnit->GetObjectGuid());
-                if (!creature || (creature && creature->IsPet()))
+                if (!creature || (creature && creature->IsPet()) || !sServerFacade.IsHostileTo(bot, creature))
                 {
                     continue;
                 }
